88049b18123ea27b7b9a4b48c40ecfa599b72c43
Markus Sundström <sundstrom.markus@gmail.com>
2018-08-09 10:35:32 +0200

    Begin UI mockup

--- src/main.c
+++ src/main.c
@@ -1,19 +1,27 @@
 
 
 #include <ncurses.h>
+#include <string.h>
+
 
 int main () {
     WINDOW *dataentry;
     int height, width;
     initscr();
+    start_color();
     raw();
     keypad(stdscr, TRUE);
     noecho();
+    init_pair(1, COLOR_YELLOW, COLOR_BLUE);
+    wbkgd(stdscr, COLOR_PAIR(1));
+    attron(A_BOLD);
+    mvprintw(0, (COLS/2)-6, "SysExCourier");
+    attroff(A_BOLD);
+    refresh();
     height = LINES - 4;
     width = COLS - 4;
     dataentry = newwin(height, width, 1, 2);
     box(dataentry, ACS_VLINE, ACS_HLINE);
-    refresh();
     wrefresh(dataentry);
     getch();
     endwin();
